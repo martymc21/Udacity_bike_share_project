a43d4331a8082980cbdfeec63eb699ab12a7168c
Marteize Howard <marteizehoward@gmail.com>
2018-05-15 07:52:42 -0500

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -8,3 +8,54 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 % matplotlib inline
+
+
+###
+###
+
+
+## Filenames
+chicago_data = pd.read_csv('Documents/chicago.csv')
+new_york_city_data = pd.read_csv('Documents/new_york_city.csv')
+washington_data = pd.read_csv('Documents/washington.csv')
+
+
+CITY_DATA = { 'chicago': 'chicago',
+              'new york city': 'new_york_city',
+              'washington': 'washington' }
+
+def get_city():
+    """
+    Asks user to choose a city and displays the data for selected city.
+
+    Returns:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+   
+   """
+    #get user input for city (chicago, new york city, washington). HINT use a while loop to handle invalid inputs
+    
+    # Ask user for input while managing incorrect input
+    while True:
+        city = input('\nHello! Let\'s explore some US bikeshare data!\n'
+                     'Would you like to see data for Chicago, New York, or Washington?\n').lower()
+        # Confirm if input string is one of the listed cities and ask again if not
+        if city not in ('chicago', 'new york', 'washington'):
+            print('\nPlease enter one of the cities listed.\n'
+                  'Returning you to the original input request:')
+        else:
+            break
+    # Use the input to select a filename
+    if city == 'chicago':
+        city_data = 'chicago.csv'
+    elif city == 'new york':
+        city_data = 'new_york_city.csv'
+    else:
+        city_data = 'washington.csv'
+
+    return city, city_data
+
+city, city_data = get_city()
+city, city_data
+    
