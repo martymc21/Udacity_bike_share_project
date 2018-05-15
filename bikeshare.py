63b0f9b8c01423662957ae62a72db58696461359
Marteize Howard <marteizehoward@gmail.com>
2018-05-14 20:30:00 -0500

    Create bikeshare.py

---
+++ bikeshare.py
@@ -0,0 +1,10 @@
+##imports
+
+from calendar import day_name, month_name
+from datetime import datetime
+from datetime import time as Time
+import time
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+% matplotlib inline
